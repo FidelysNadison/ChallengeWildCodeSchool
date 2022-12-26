05308d1ef724d6bb6b5978867d9e93f89478cc8b
FidelysNadison <nadisonsolojeanfidelys@gmail.com>
2022-12-26 22:19:05 +0100

    rectification des components

---
+++ les-argonautes/src/components/Liste.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Liste() {
+  return (
+    <main>
+      <h2>Membres de l'équipage</h2>
+      <section class="member-list">
+        <div class="member-item">Eleftheria</div>
+        <div class="member-item">Gennadios</div>
+        <div class="member-item">Lysimachos</div>
+      </section>
+    </main>
+  );
+}
