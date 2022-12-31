61929d7e8df457aa3160bc04087b8c0d59f197bf
Fidelys <nadisonsolojeanfidelys@gmail.com>
2022-12-31 18:24:09 +0100

    correction des erreurs

--- les-argonautes/src/components/Liste.js
+++ les-argonautes/src/components/Liste.js
@@ -16,20 +16,6 @@ function Liste() {
     console.log(...todos);
   };
 
-  const updateTodo = (todoId, newValue) => {
-    if (!newValue.text || /^\s*$/.test(newValue.text)) {
-      return;
-    }
-
-    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
-  };
-
-  const removeTodo = id => {
-    const removedArr = [...todos].filter(todo => todo.id !== id);
-
-    setTodos(removedArr);
-  };
-
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
       if (todo.id === id) {
--- les-argonautes/src/components/Todo.js
+++ les-argonautes/src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Todo = ({ todos, completeTodo }) => {
   return todos.map((todo, index) => (
